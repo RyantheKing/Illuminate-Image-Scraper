bda886f8971575790dad0e8f8553f0d9845f2ca1
not a cat <pandinobud@gmail.com>
2020-05-06 00:17:30 +0000

    Initial commit

---
+++ funcs.py
@@ -0,0 +1,24 @@
+import requests
+import shutil
+
+def iterate(maximum): 
+    blank = None
+
+    for i in range(maximum): 
+        # This is the image url.
+        image_url = ("https://imagebank.illuminateed.com/imagebank/" + str(i))
+        # Open the url image, set stream to True, this will return the stream content.
+        resp = requests.get(image_url, stream=True)
+
+        # Open a local file with wb ( write binary ) permission.
+
+        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+        resp.raw.decode_content = True
+
+        # Copy the response stream raw data to local image file. 
+
+        if blank is None: 
+            blank = resp.content
+        elif resp.content != blank: 
+            with open((str(i)+'.jpg'), 'wb+') as local_file: 
+                shutil.copyfileobj(resp.raw, local_file) 
---
+++ main.py
@@ -0,0 +1,48 @@
+# Import requests, shutil python module.
+import os
+import funcs
+
+maximum = 10000000
+
+mode = input('enter the mode (r to delete images, w to download images): ').lower() 
+
+print('e') 
+
+if mode == 'r': 
+    for path in os.listdir(): 
+        name, *ext = path.split('.') 
+
+        if ext and ext[0] == 'jpg': 
+            os.remove(path) 
+
+elif mode == 'w': 
+    funcs.iterate(maximum) 
+
+    '''
+    for i in range(maximum): 
+    # This is the image url.
+        image_url = ("https://imagebank.illuminateed.com/imagebank/" + str(i))
+        # Open the url image, set stream to True, this will return the stream content.
+        resp = requests.get(image_url, stream=True)
+
+        # Open a local file with wb ( write binary ) permission.
+        local_file = open((str(i)+'.jpg'), 'wb+')
+
+        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+        resp.raw.decode_content = True
+
+        # Copy the response stream raw data to local image file.
+        shutil.copyfileobj(resp.raw, local_file)
+
+        # Remove the image url response object.
+        del resp
+        
+        #if blank == open((str(i)+'.jpg'), 'rb').read():
+        #   os.remove((str(i)+".jpg"))
+        #print(blank != open((str(i)+'.jpg'), 'rb').read(), i)
+
+        try:
+            Image.open((str(i)+'.jpg'))
+        except:
+            os.remove((str(i)+".jpg")) 
+    ''' 
