8be19c94bcc0123a8477c65663cef7969cb3eb21
not a cat <32406395+the-non-feline@users.noreply.github.com>
2020-05-06 14:55:43 -0700

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,4 @@
+minimum = 1 # minimum image id
+maximum = 1000 # maximum image id + 1
+every = 10 # number of images each thread will handle
+directory = 'test images/' # where to save the files (THE PROGRAM WILL NOT AUTOMATICALLY CREATE THE FOLDER) 
