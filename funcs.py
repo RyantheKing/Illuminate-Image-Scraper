f68651c42c080652a9d439eae8ea79da16346fc2
not a cat <pandinobud@gmail.com>
2020-05-05 21:30:02 -0700

    [fix] handles network error now

--- funcs.py
+++ funcs.py
@@ -7,32 +7,31 @@ blank = None
 def search(to_search, interval): 
     global blank
 
-    count = 0
-
     for i in to_search: 
         # This is the image url.
         image_url = ("https://imagebank.illuminateed.com/imagebank/" + str(i))
         # Open the url image, set stream to True, this will return the stream content.
-        resp = requests.get(image_url, stream=True)
+        try: 
+            resp = requests.get(image_url, stream=True) 
+        except requests.ConnectionError: 
+            print(f'error on number {i}') 
+        else: 
+            # Open a local file with wb ( write binary ) permission.
 
-        # Open a local file with wb ( write binary ) permission.
+            # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+            resp.raw.decode_content = True
 
-        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
-        resp.raw.decode_content = True
+            # Copy the response stream raw data to local image file. 
 
-        # Copy the response stream raw data to local image file. 
+            if blank is None: 
+                blank = resp.content
+            elif resp.content != blank: 
+                with open((f'{i}.jpg'), 'wb+') as local_file: 
+                    #print(resp.content)  
 
-        if blank is None: 
-            blank = resp.content
-        elif resp.content != blank: 
-            with open((f'{i}.jpg'), 'wb+') as local_file: 
-                #print(resp.content)  
+                    #shutil.copyfileobj(resp.raw, local_file) 
 
-                #shutil.copyfileobj(resp.raw, local_file) 
-
-                local_file.write(resp.content) 
-        
-        count += 1
+                    local_file.write(resp.content) 
     
     print('done') 
 
--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 import os
 import funcs
 
-minimum = 1
-maximum = 1000
-every = 100
+minimum = 100000
+maximum = 200000
+every = 1000
 interval = 100
 
 mode = input('enter the mode (r to delete images, w to download images): ').lower() 
