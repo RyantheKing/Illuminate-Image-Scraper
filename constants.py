13eaeb57a4a8c665c130131ecb68b0d3eb9c4edc
not a cat <pandinobud@gmail.com>
2020-05-05 23:00:20 -0700

    comments

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-minimum = 1
-maximum = 1000
-every = 1
-directory = 'test images/' 
+minimum = 1 # minimum image id
+maximum = 1000 # maximum image id + 1
+every = 1 # number of images each thread will handle (it's probably best to leave this at 1) 
+directory = 'test images/' # where to save the files (THE PROGRAM WILL NOT AUTOMATICALLY CREATE THE FOLDER) 
