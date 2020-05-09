5856542f387574fd9eaae7a850e11296453335e2
not a cat <pandinobud@gmail.com>
2020-05-09 14:53:19 -0700

    some final logging stuff

--- constants.py
+++ constants.py
@@ -3,4 +3,4 @@ maximum = 1000 # maximum image id + 1
 every = 10 # number of images each thread will handle
 directory = 'test images/' # where to save the files (THE PROGRAM WILL NOT AUTOMATICALLY CREATE THE FOLDER) 
 timeout = 60 # number of seconds before a request times out
-log_file = 'logs.txt' 
+log_file = None
--- logging_stuff.py
+++ logging_stuff.py
@@ -1,13 +1,14 @@
 import logging
+import sys
 
 logger = logging.getLogger(__name__) 
 
 logger.setLevel(logging.DEBUG) 
 
+logger.addHandler(logging.StreamHandler(sys.stdout)) 
+
 def add_file(file): 
     logger.addHandler(logging.StreamHandler(file)) 
 
 def debug(msg, *args, **kwargs): 
-    print(msg) 
-    
     logger.debug(str(msg), *args, **kwargs) 
